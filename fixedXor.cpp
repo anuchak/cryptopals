05e34e4f13da686eef117b32b6a11c137d11d591
Anurag Chakraborty <anurag.chakraborty98@gmail.com>
2019-09-29 21:14:43 +0530

    Update fixedXor.cpp

--- fixedXor.cpp
+++ fixedXor.cpp
@@ -1,27 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() 
+int main()
 {
   string a,b;
-  cin>>a>>b;
+  a = "1c0111001f010100061a024b53535009181c";
+  b = "686974207468652062756c6c277320657965";
   for(int i = 0; i < a.size(); i++)
   {
-    int f,s;
-    if(a[i] >= 'a' && a[i] <= 'f')
-    {
-      f = a[i]-'a'+10;
-    }
-    else
-      f = a[i] - '0';
-    if(b[i] >= 'a' && b[i] <= 'f')
-    {
-      s = b[i]-'a'+10;
-    }
-    else
-      s = b[i] - '0';
-    int res = f^s;
-    cout << hex << res;
+    int j = (a[i] <= '9') ? (a[i] - '0') : (a[i] - 'a' + 10);
+    int k = (b[i] <= '9') ? (b[i] - '0') : (b[i] - 'a' + 10);
+    int m = j ^ k;
+    cout << m;
   }
-  cout<<endl;
+  return 0;
 }
